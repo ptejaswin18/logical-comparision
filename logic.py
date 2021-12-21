2b53302887895a73d3ea76da4bedd13f6a5b75ad
ptejaswin18 <96372320+ptejaswin18@users.noreply.github.com>
2021-12-22 00:09:12 +0530

    logic.py

---
+++ logic.py
@@ -0,0 +1,29 @@
+# using  'and' logical operator
+a=int(input("enter your a:"))
+
+if a>=4 and a<=5:
+    print("true")
+else:
+    print("false")
+
+
+
+
+
+
+
+#using 'not' operator
+voterage=int(input("enter age"))
+if not voterage>=18:
+    print("not eligible for voteing")
+else:
+    print(" eligible for voteing")    
+    
+    
+    
+    
+ 
+# comparision operator 
+x = 10
+y = 13
+print(x != y)
